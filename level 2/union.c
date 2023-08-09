544643f7caa77332bc08b96a4a5e3aec481c737d
Burak <100238698+Ropnez@users.noreply.github.com>
2023-08-09 18:14:29 +0300

    Update

--- level 2/union.c
+++ level 2/union.c
@@ -1,38 +1,30 @@
 #include <unistd.h>
-
-int	check(char *str, char c, int n)
+int check (int c, char *str, int index)
 {
-	int	i = 0;
-	while (i < n)
-	{
-		if (str[i] == c)
-			return (0);
-		i++;
-	}
-	return (1);
+    int i = 0;
+    while (i < index)
+    {
+        if (str [i] == c)
+            return(0);
+        i++;
+    }
+    return(1);
 }
-
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
-	int i = 0, j = 0;
-
-	if (ac == 3)
-	{
-		while (av[1][++i]);
-		while (av[2][j])
-		{
-			av[1][i] = av[2][j];
-			i++;
-			j++;
-		}
-		av[1][i] = '\0';
-		i = 0;
-		while (av[1][i])
-		{
-			if (check(av[1], av[1][i], i))
-				write(1, &av[1][i], 1);
-			i++;
-		}
-	}
-	write(1, "\n", 1);
+    int i = 0, j = 0, k = 0; 
+    if (ac == 3)
+    {
+        while (av[1][i])
+            i++;
+        while (av[2][j])
+            av[1][i++] = av[2][j++];
+        while (k <= i)
+        {
+            if(check(av[1][k], av[1], k))
+                write(1, &av[1][k], 1);
+            k++;
+        }
+    }
+    write(1, "\n", 1);
 }
