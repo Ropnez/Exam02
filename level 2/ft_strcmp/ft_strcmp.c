4614032b9717c22246bd1815583e71efa7447920
Burak <100238698+Ropnez@users.noreply.github.com>
2023-08-11 01:10:35 +0300

    new update

--- level 2/ft_strcmp/ft_strcmp.c
+++ level 2/ft_strcmp/ft_strcmp.c
@@ -1,12 +1,7 @@
 int    ft_strcmp(char *s1, char *s2)
 {
-    int i = 0;
-
-    while (s1[i] && s2[i])
-    {
-        if(s1[i] != s2[i])
-            return s1[i] - s2[i];
-        i++;
-    }
-    return 0;
+	int i = 0;
+	while(s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return(s1[i] - s2[i]);
 }
